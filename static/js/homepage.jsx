6967444eb770535dea737b1c6acfe85e6d1b9f90
Athelia Crosmun <athelia.hk@gmail.com>
2020-10-20 17:53:03 -0400

    Add greeting, image, and link to cards on homepage

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -3,7 +3,9 @@
 function Homepage() {
   return (
     <div>
-      This component needs to be implemented.
+      <h2>These cards are super cool!</h2>
+      <p><a href="/cards">See the cards</a></p>
+      <img src="/static/img/balloonicorn.jpg" />
     </div>
   );
 }
