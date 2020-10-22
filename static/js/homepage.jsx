f8f564ad0db7bb5b2965b21cb4be3896cee2782d
athelia <athelia.hk@gmail.com>
2020-10-22 18:54:00 -0400

    Add about page, link, route

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -5,6 +5,7 @@ function Homepage() {
     <React.Fragment>
       <h2>These cards are super cool!</h2>
       <p><a href="/cards">See the cards</a></p>
+      <p><a href="/about">About the author</a></p>
       <img src="/static/img/balloonicorn.jpg" />
     </React.Fragment>
   );
