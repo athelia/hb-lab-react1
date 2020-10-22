41663e0e3d94fbce6132a3f5269aa2ba0d1d2747
Athelia Crosmun <athelia.hk@gmail.com>
2020-10-22 17:30:24 -0400

    Create TradingCardContainer and remove individual trading cards

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -4,8 +4,51 @@ const tradingCardData = [
     name: 'Balloonicorn',
     skill: 'video games',
     imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpg'
+  },
+
+  {
+    name: 'Seed.py',
+    skill: 'making curry dishes',
+    imgUrl: '/static/img/seedpy.jpg'
+  },
+
+  {
+    name: 'Polymorphism',
+    skill: 'costumes',
+    imgUrl: '/static/img/polymorphism.jpg'
+  },
+
+  {
+    name: 'Short Stack Overflow',
+    skill: 'ocean animal trivia',
+    imgUrl: '/static/img/shortstack-overflow.jpg'
+  },
+
+  {
+    name: 'Merge',
+    skill: 'bullet journaling',
+    imgUrl: '/static/img/merge.jpg'
   }
-]
+];
 
 function TradingCard(props) {
   return (
@@ -28,46 +71,22 @@ ReactDOM.render(
   document.querySelector('#balloonicorn')
 );
 
+// for (card of tradingCardData){
+//   ReactDOM.render(
+//     (
+//       <TradingCard
+//         name={card.name}
+//         skill={card.skill}
+//         imgUrl="/static/img/float.jpg"
+//       />
+//     ),
+//     document.querySelector('#float')
+//   );
+// }
+  
+function TradingCardContainer(){
+  return (<React.Fragment>Success!</React.Fragment>)
+}
 ReactDOM.render(
-  (
-    <TradingCard
-      name="Float"
-      skill="baking pretzels"
-      imgUrl="/static/img/float.jpg"
-    />
-  ),
-  document.querySelector('#float')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Llambda"
-      skill="knitting scarves"
-      imgUrl="/static/img/llambda.jpg"
-    />
-  ),
-  document.querySelector('#llambda')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Merge"
-      skill="hosting dance parties"
-      imgUrl="/static/img/merge.jpg"
-    />
-  ),
-  document.querySelector('#merge')
-)
-
-ReactDOM.render(
-  (
-    <TradingCard 
-      name="Off-By-One"
-      skill="making flower crowns"
-      imgUrl="/static/img/off-by-one.jpg"
-    />
-  ), 
-  document.querySelector('#off-by-one')
+  <TradingCardContainer />, document.querySelector('#trading-card-container')
 )
